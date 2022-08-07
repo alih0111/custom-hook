f9fd84aba8e6cbed2b32c2ac2b10f41629092cc9
#a.H7527 <hyyddyy@gmail.com>
2022-08-07 11:38:11 +0430

    add useDebugValue

--- src/useFormFileds.jsx
+++ src/useFormFileds.jsx
@@ -1,15 +1,18 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
+import { useDebugValue } from "react";
 
 export default function useFormFileds(init) {
-    const [fileds, setFileds] = useState(init);
-    
-      function handleChange(e) {
-        const { target } = e;
-        setFileds({
-          ...fileds,
-          [target.name]: target.value,
-        });
-      }
+  const [fileds, setFileds] = useState(init);
 
-      return{fileds,handleChange}
+  function handleChange(e) {
+    const { target } = e;
+    setFileds({
+      ...fileds,
+      [target.name]: target.value,
+    });
+  }
+
+  useDebugValue(fileds, (fileds) => `${Object.keys(fileds).length} fileds`);
+
+  return { fileds, handleChange };
 }
